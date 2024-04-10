270df9344bde4272faac17fd425bd60acbd19dac
Hoang <144702750+DucHoanghust@users.noreply.github.com>
2024-04-10 17:58:06 +0700

    Update run_normal_db15k.sh

--- scripts/run_normal_db15k.sh
+++ scripts/run_normal_db15k.sh
@@ -1,7 +1,7 @@
 MARGIN=12
 KERNEL=transe
 
-CUDA_VISIBLE_DEVICES=0 nohup python run.py -dataset=DB15K \
+CUDA_VISIBLE_DEVICES=0 nohup python /content/MANS/run.py -dataset=DB15K \
   -num_batch=400 \
   -margin=$MARGIN \
   -neg_mode=normal \
@@ -13,4 +13,4 @@ CUDA_VISIBLE_DEVICES=0 nohup python run.py -dataset=DB15K \
   -img_dim=4096 \
   -neg_num=1 \
   -kernel=transe \
-  -learning_rate=1.0 > ./log/DB15K-base-$KERNEL-margin$MARGIN.txt &
+  -learning_rate=1.0 > /content/MANS/log/DB15K-base-$KERNEL-margin$MARGIN.txt &
