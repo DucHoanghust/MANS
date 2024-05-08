04591e8a1544ad4bf5ba5ab7537f0adc56fdfd39
Hoang <144702750+DucHoanghust@users.noreply.github.com>
2024-05-08 21:35:49 +0700

    Update run_normal_db15k.sh

--- scripts/run_normal_db15k.sh
+++ scripts/run_normal_db15k.sh
@@ -6,7 +6,7 @@ CUDA_VISIBLE_DEVICES=0 nohup python /content/MANS/run.py -dataset=DB15K \
   -margin=$MARGIN \
   -neg_mode=normal \
   -train_mode=normal \
-  -epoch=1000 \
+  -epoch=100 \
   -save=/content/MANS/checkpoint/FB15K-base-$KERNEL-margin$MARGIN \
   -test_mode=lp \
   -img_grad=False \
